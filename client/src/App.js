8f794482216e791048b2cb23d73b7ffc6fdb7026
Christina Sunnegårdh <christinasunnegardh@gmail.com>
2020-03-26 13:30:06 +0100

    easter eggs

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const trivia = {
+  image: "https://www.partyhallen.se/cache/0e/799x799-b_paskagg-jumbo-40cm-1.jpg",
+  title: "Hmm.. I wonder what the answers to these questions are?",
+  questions: [
+    "How many companies use Docker?",
+    "How many total repos are there on Docker Hub?", 
+    "The company behind Docker is called Docker, Inc. But that hasn't always been their name. What was Docker, Inc. called before?",
+    "How many total pulls have been done on Docker Hub?"
+  ]
+}
+
 function App() {
-  let [apiResponse, setApiResponse] = useState()
+  let [apiResponse, setApiResponse] = useState({title: "", description: "", question1: "", answer1: ""})
   useEffect(() => {
     callAPI()
   },[])
 
   const callAPI = () => {
-    fetch("http://localhost:9000")
-        .then(res => res.text())
+    fetch("http://localhost:9000/trivia")
+        .then(res => res.json())
         .then(res => setApiResponse(res))
-        
+        .catch(() => setApiResponse(trivia))
   }
 
   return (
     <div className="App">
-        <p className="App-text">{apiResponse}</p>
+        <img className="easter-eggs" src={apiResponse.image}/>
+        <h2>{apiResponse.title}</h2>
+        <h3>{apiResponse.description}</h3>
+        {apiResponse.questions && apiResponse.questions.map(question => (
+          <div className="question">{question}</div>
+        ))}
+        {apiResponse.questionsAnswers && apiResponse.questionsAnswers.map(obj => (
+          <div>
+            <div className="question">{obj.question}</div>
+            <div className="answer">{obj.answer}</div>
+          </div>
+        ))}
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -5,6 +5,20 @@ const port = 9000
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Hi from backend!'))
+const triviaAnswers = {
+    image: "https://axonstatic.se/items/420/7793.jpg",
+    title: "You found the answers!", 
+    description: "Good job! Both your frontend and backend are running in separate containers. Here are the answers to the questions!", 
+    questionsAnswers: [
+        {question: "How many companies use Docker?", answer: "Over 30 000! That's a lot of companies."},
+        {question: "How many total repos are there on Docker Hub?", answer: "6 million!"},
+        {question: "The company behind Docker is called Docker, Inc. But that hasn't always been their name. What was Docker, Inc. called before?", answer: "The were called dotCloud, Inc. Before Docker, dotCloud (a second generation PaaS platform) was their largest product!"},
+        {question: "How many total pulls have been done on Docker Hub?", answer: "Astonishing 130 billion times..."}
+      ],
+    sources: ["https://enlyft.com/tech/products/docker", "en annan källa"]
+}
+
+app.get('/', (req, res) => res.send('Your backend is up and running!'))
+app.get('/trivia', (req, res) => res.send(triviaAnswers))
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
